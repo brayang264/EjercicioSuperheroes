a78f0c0e95a24a4bbf0b0f046e73db995dfca20e
Brayan Gómez <brayan.gomez1@udea.edu.co>
2023-10-07 17:54:53 -0500

    Se agrego metodo para generar nombres de super heroes de manera aleatoria y se corrigió un bug con la generación aleatoria de personas

--- SuperAmigos/src/main/java/Control/Validate.java
+++ SuperAmigos/src/main/java/Control/Validate.java
@@ -2,17 +2,42 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Control;
+package Model;
 
-import Model.Hero;
-import Model.Person;
+import java.awt.Dimension;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.ScrollPaneConstants;
 
 /**
  *
- * @author brayan.gomez1
+ * @author Admin
  */
-public interface Validate {
-    public boolean isHero(Person person);
-    public Hero createHero(Person person, String artefacto, int force, int see);
-    
+public class Validate {
+    //Convierte el string a float, en caso de que no se pueda convertir devuleve un 0
+    public float convertToFloat(String number){
+        return (!isANumber(number))?-1: Float.parseFloat(number);
+    }
+    //Se comprueba si se pude convertir al string a float.
+    public boolean isANumber(String number){
+        try{
+            Float.parseFloat(number);
+            return true;
+        }catch(NumberFormatException e){
+            return false;
+        }
+    }
+    public void print(String text){
+        JTextArea textArea = new JTextArea(10, 40);
+        textArea.setText(text);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+
+        // Configura el cuadro de diálogo personalizado
+        scrollPane.setPreferredSize(new Dimension(400, 200));
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+        // Muestra el cuadro de diálogo personalizado
+        JOptionPane.showMessageDialog(null, scrollPane, text, JOptionPane.INFORMATION_MESSAGE);
+    }
 }
